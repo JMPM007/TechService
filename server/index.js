21a53e9f6055cbe9dd5baa21585d60d410e85761
juan miguel <juan_miguel.perdomo@uao.edu.co>
2026-09-05 17:05:13 -0500

    base de la bd y conexion con node.js

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import pool from "./db.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api/test-db", async(req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM usuarios")
+        res.json({mensaje: "Conexion a la BD exitosa", usuarios: rows})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor ejecutandose en http://localhost:3000")
+})
